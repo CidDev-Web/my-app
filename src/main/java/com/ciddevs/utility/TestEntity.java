024333b6c060bbbb029ba30e76dff33a7754cf0a
ciddev <ciddev.web@gmail.com>
2021-03-31 01:48:43 +0200

    Subida final

---
+++ src/main/java/com/ciddevs/utility/TestEntity.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package com.ciddevs.utility;
+
+/**
+ * @author miguelcid
+ *
+ */
+public class TestEntity {
+	
+	/**
+	 * Propiedad de prueba
+	 */
+	private String nombre;
+
+	/**
+	 * Getter
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+	
+	/**
+	 * Setter
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+}
